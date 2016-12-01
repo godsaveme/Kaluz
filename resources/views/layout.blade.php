0c188005119f836e55025ee5dab3e101fe0a2cb5
mdiazr <mdiaz@honeysoft.pe>
2016-11-30 22:16:45 -0500

    cmabioColor

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -48,9 +48,9 @@
         <!-- Logo -->
         <a href="/" class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini"><b>SL</b>F</span>
+          <span class="logo-mini"><b>KL</b>Z</span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>Sales</b>Fly</span>
+          <span class="logo-lg"><b>Kaluz</b>Store</span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top" role="navigation">
